bf820193f8c7ec08fb10977e2a9f91a9855b7174
Catalinpa19 <palade.catalin30@yahoo.com>
2026-07-10 10:49:14 +0300

    Created AuctionItem.cs
    Implement basic AuctionItem model..

---
+++ BackEnd/Backend/Backend/Models/AuctionItem.cs
@@ -0,0 +1,57 @@
+﻿namespace Backend.Models
+{
+    public class AuctionItem
+    {
+        public int ID { get; set; }
+
+
+        public string Name { get; set; }
+
+
+        public decimal StartPrice { get; set; }
+
+        public decimal CurrentPrice { get; set; }
+
+
+        //TO DO IMPLEMENT CATEGORY CLASS
+
+
+        //public Category Category {get;set;}
+
+
+        public string Description { get; set; }
+
+
+        public string Location { get; set; }
+
+
+        //TO DO IMPLEMENT USER CLASS
+
+
+        //public User User{get;set;}
+
+        //public User Winner { get; set; }
+
+        public enum Status
+        {
+            Added,
+            Validated,
+            ActiveBid,
+            NoWinner,
+            Sold,
+
+        }
+
+        public DateTime StartDate;
+
+        public DateTime EndDate;
+
+
+
+
+        //TO ADD PHOTOLIST
+
+
+
+     }
+}
