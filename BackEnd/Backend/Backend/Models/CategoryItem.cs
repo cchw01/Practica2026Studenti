9d9e8eac69ac50511bd4cf6984e97b3dc86df7c5
Daniela BURGHIU <burghiudaniela2005@gmail.com>
2026-07-10 11:55:51 +0300

    Creare clasa CategoryItem in Models

---
+++ BackEnd/Backend/Backend/Models/CategoryItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Models
+{
+    public class CategoryItem
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string items { get; set; }// List<item>
+    }
+}
