1a2966c8a12b06b075d381c0dc7972e7a9cded2a
NedelcuRadu1 <nedelcuradu66@gmail.com>
2026-07-10 13:30:24 +0300

    feat: initialize AppModule with core configurations, routing, and authentication components

--- frontend/LicitatieFrontend/src/app/app-module.ts
+++ frontend/LicitatieFrontend/src/app/app-module.ts
@@ -1,27 +1,22 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { ProfilePage } from './profile/profile-page/profile-page';
-
 import { RegisterPage } from './menu-item/register-page/register-page';
 import { LoginPage } from './menu-item/login-page/login-page';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [App, LoginPage, RegisterPage],
+  declarations: [App, ProfilePage, LoginPage, RegisterPage],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatButtonModule,
+    FormsModule,
     ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
   ],
-
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
