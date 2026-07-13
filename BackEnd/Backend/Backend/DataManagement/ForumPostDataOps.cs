a5ab786cdc7377ca9e5308116c75f611048f7cff
Bulgaru Bogdan <bulgarubogdan11@gmail.com>
2026-07-13 11:03:48 +0300

    Added CRUD operations for Forum Post

---
+++ BackEnd/Backend/Backend/DataManagement/ForumPostDataOps.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Backend.Models;
+namespace Backend.DataManagement
+{
+    public class ForumPostDataOps
+    {
+
+        private readonly ApplicationDbContext DbContext;
+
+        public ForumPostDataOps(ApplicationDbContext context)
+        {
+            DbContext = context;
+        }
+
+        public ForumPost[]? GetForumPosts()
+        {
+            return DbContext?.ForumPosts;
+        }
+
+        public ForumPost? GetForumPostById(int id)
+        {
+            return DbContext?.ForumPosts.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public void AddForumPost(ForumPost forumPost)
+        {
+            DbContext?.ForumPosts.Add(forumPost);
+            DbContext?.SaveChanges();
+        }
+
+        public void DeleteForumPost(int id)
+        {
+            DbContext?.ForumPosts.Remove.Where(x = > x.Id == id).FirstOrDefault();
+        }
+
+        public void UpdateForumPost(ForumPost forumPost)
+        {
+            DbContext?.ForumPosts.Update(forumPost);
+            DbContext?.SaveChanges();
+        }
+
+    }
+}
