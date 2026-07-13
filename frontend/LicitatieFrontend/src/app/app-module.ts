39c8fd08f638b94893c48cd07abb204333723edd
Daniela BURGHIU <burghiudaniela2005@gmail.com>
2026-07-13 13:00:34 +0300

    contact-page (#28)
    * contact-page
    
    * contact-page-no-styling
    
    ---------
    
    Co-authored-by: Noelia <132777122+noesobc@users.noreply.github.com>

--- frontend/LicitatieFrontend/src/app/app-module.ts
+++ frontend/LicitatieFrontend/src/app/app-module.ts
@@ -8,23 +8,19 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 
-
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { HomePage } from './home-page/home-page';
 import { ProfilePage } from './profile/profile-page/profile-page';
 import { RegisterPage } from './menu-item/register-page/register-page';
 import { LoginPage } from './menu-item/login-page/login-page';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 
+import { ContactPage } from './menu-item/contact-page/contact-page';
 
 @NgModule({
-  declarations: [App, LoginPage, RegisterPage, HomePage, ProfilePage],
+  declarations: [App, LoginPage, RegisterPage, HomePage, ProfilePage, ContactPage],
   imports: [
-   BrowserModule,
+    BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
