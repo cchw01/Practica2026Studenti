a668d75021293d2ba27988f117e0928a39ec188e
Alexandru Taranu <alexandru.taranu@student.unitbv.ro>
2026-07-13 13:29:55 +0300

    Auctions_Update

--- frontend/LicitatieFrontend/src/app/app-module.ts
+++ frontend/LicitatieFrontend/src/app/app-module.ts
@@ -1,30 +1,25 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-
-
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { HomePage } from './home-page/home-page';
 import { ProfilePage } from './profile/profile-page/profile-page';
 import { RegisterPage } from './menu-item/register-page/register-page';
 import { LoginPage } from './menu-item/login-page/login-page';
-import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-
+import { Add } from './menu-item/add/add';
+import { HttpClientModule } from '@angular/common/http';
+import { AuctionsPage } from './auctions-page/auctions-page';
 
 @NgModule({
-  declarations: [App, LoginPage, RegisterPage, HomePage, ProfilePage],
+  declarations: [App, LoginPage, RegisterPage, HomePage, ProfilePage, Add, AuctionsPage],
   imports: [
-   BrowserModule,
+    BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
@@ -32,6 +27,7 @@ import { MatInputModule } from '@angular/material/input';
     MatIconModule,
     MatFormFieldModule,
     MatInputModule,
+    HttpClientModule,
   ],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
--- frontend/LicitatieFrontend/src/app/app-routing-module.ts
+++ frontend/LicitatieFrontend/src/app/app-routing-module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfilePage } from './profile/profile-page/profile-page';
-
 import { RegisterPage } from './menu-item/register-page/register-page';
 import { LoginPage } from './menu-item/login-page/login-page';
 import { HomePage } from './home-page/home-page';
+import { AuctionsPage } from './auctions-page/auctions-page';
 
 const routes: Routes = [
 
   {path: 'home-page', component: HomePage},
   { path: 'login-page', component: LoginPage },
   { path: 'register-page', component: RegisterPage },
-  { path: '', redirectTo: '/register', pathMatch: 'full' }
+  { path: '', redirectTo: '/register', pathMatch: 'full' },
+  { path: 'auctions', component: AuctionsPage },
 
 ];
 
