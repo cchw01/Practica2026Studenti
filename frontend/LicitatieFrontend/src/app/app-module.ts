638b4661344ceac649763228be09d7de76f786dd
Gabriela-Petcu <petcugabrielai13@gmail.com>
2026-07-13 12:39:13 +0300

    added review frontend

--- frontend/LicitatieFrontend/src/app/app-module.ts
+++ frontend/LicitatieFrontend/src/app/app-module.ts
@@ -1,21 +1,15 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { ReviewComponent} from './Models/review/review';
 
 @NgModule({
-  declarations: [
-    App,
-    
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-  ],
-  bootstrap: [App]
+  declarations: [App],
+  imports: [BrowserModule, AppRoutingModule, ReviewComponent],
+  providers: [provideBrowserGlobalErrorListeners(), provideAnimationsAsync()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
