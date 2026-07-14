0db30a53e3abe6f530cbe8ba26a2692780c35eb8
Hanna Katai <hanna01.katai@gmail.com>
2026-07-14 12:40:35 +0300

    import_module

--- frontend/LicitatieFrontend/src/app/app-module.ts
+++ frontend/LicitatieFrontend/src/app/app-module.ts
@@ -3,20 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing-module';
-
 import { App } from './app';
 import { ProfilePage } from './profile/profile-page/profile-page';
 import { RegisterPage } from './menu-item/register-page/register-page';
 import { LoginPage } from './menu-item/login-page/login-page';
+import { HomePage } from './home-page/home-page';
 import { ContactPage } from './menu-item/contact-page/contact-page'
 import { ForumPage } from './forum-page/forum-page'
 //import { Add } from './menu-item/add/add';
 import { AuctionsPage } from './auctions-page/auctions-page';
-import { ContactPage } from './menu-item/contact-page/contact-page';
-import { ForumPage } from './forum-page/forum-page';
 import { ReviewComponent } from './Models/review/review';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 //am scos Add din declarations
 @NgModule({
