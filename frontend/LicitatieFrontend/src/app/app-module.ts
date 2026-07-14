14a23a011624e6b6bf2e7337dabe0e6d42f294df
Gabriela-Petcu <petcugabrielai13@gmail.com>
2026-07-14 11:58:22 +0300

    Review data model (#44)
    * added review frontend
    
    * quick changes in review model
    
    * ApplicationDataBase fixed
    
    Removed hard-coded database
    
    * Fixed bugs and path
    
    * Eliminare folder nefolosit
    
    * feat : relationships (item-> category, owner, winner)
    
    * added review page to pages
    
    ---------
    
    Co-authored-by: IacobGiulia <giuliaiacob004@gmail.com>
    Co-authored-by: Catalinpa19 <palade.catalin30@yahoo.com>
    Co-authored-by: Ca <ioana.cambur@student.unitbv.ro>
    Co-authored-by: Ionita_Vlad <ionita.vlad2006@gmail.com>
    Co-authored-by: NedelcuRadu1 <nedelcuradu66@gmail.com>
    Co-authored-by: Peafowll <tudorpaun16@gmail.com>
    Co-authored-by: NedelcuRadu1 <161981241+NedelcuRadu1@users.noreply.github.com>

--- frontend/LicitatieFrontend/src/app/app-module.ts
+++ frontend/LicitatieFrontend/src/app/app-module.ts
@@ -1,16 +1,12 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { AppRoutingModule } from './app-routing-module';
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing-module';
+
 import { App } from './app';
-import { HomePage } from './home-page/home-page';
 import { ProfilePage } from './profile/profile-page/profile-page';
 import { RegisterPage } from './menu-item/register-page/register-page';
 import { LoginPage } from './menu-item/login-page/login-page';
@@ -18,6 +14,9 @@ import { ContactPage } from './menu-item/contact-page/contact-page'
 import { ForumPage } from './forum-page/forum-page'
 //import { Add } from './menu-item/add/add';
 import { AuctionsPage } from './auctions-page/auctions-page';
+import { ContactPage } from './menu-item/contact-page/contact-page';
+import { ForumPage } from './forum-page/forum-page';
+import { ReviewComponent } from './Models/review/review';
 
 //am scos Add din declarations
 @NgModule({
@@ -27,14 +26,19 @@ import { AuctionsPage } from './auctions-page/auctions-page';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    MatFormFieldModule,
-    MatButtonModule,
+    CommonModule,
+    HttpClientModule,
     MatCardModule,
     MatIconModule,
+    MatFormFieldModule,
     MatInputModule,
-    HttpClientModule,
+    MatButtonModule,
+    ReviewComponent,
   ],
-  providers: [provideBrowserGlobalErrorListeners()],
-  bootstrap: [App],
+
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+  ],
+  bootstrap: [App]
 })
 export class AppModule {}
