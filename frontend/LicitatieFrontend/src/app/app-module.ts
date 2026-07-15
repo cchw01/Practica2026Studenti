e4dbef119082ed179a55018900eddf6dc0a7c94b
Matei50 <matei.tr50@gmail.com>
2026-07-15 11:38:30 +0300

    Adaugare formular de login si configurare Angular Material

--- frontend/LicitatieFrontend/src/app/app-module.ts
+++ frontend/LicitatieFrontend/src/app/app-module.ts
@@ -8,6 +8,10 @@ import { RegisterPage } from './menu-item/register-page/register-page';
 import { LoginPage } from './menu-item/login-page/login-page';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   declarations: [App, LoginPage, RegisterPage],
   imports: [
