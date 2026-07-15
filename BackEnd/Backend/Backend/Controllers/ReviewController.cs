52365bd342d0a04b6f923dbd2bdbd09053bd2ed1
NedelcuRadu1 <nedelcuradu66@gmail.com>
2026-07-15 11:40:50 +0300

    Fixed app-module

--- BackEnd/Backend/Backend/Controllers/ReviewController.cs
+++ BackEnd/Backend/Backend/Controllers/ReviewController.cs
@@ -1,8 +1,4 @@
-
-
-using Backend.Data;
 using Backend.DataManagement;
-﻿using Backend.DataManagement;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Backend.Models;
--- frontend/LicitatieFrontend/src/app/app-module.ts
+++ frontend/LicitatieFrontend/src/app/app-module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -10,14 +12,16 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { ProfilePage } from './menu-item/profile-page/profile-page';
 import { RegisterPage } from './menu-item/register-page/register-page';
 import { LoginPage } from './menu-item/login-page/login-page';
 import { ContactPage } from './menu-item/contact-page/contact-page';
+import { HomePage } from './home-page/home-page';
+import { AuctionsPage } from './auctions-page/auctions-page';
+import { ForumPage } from './forum-page/forum-page';
+import { ReviewComponent } from './Models/review/review';
 
 import { AuthInterceptor } from './services/auth-interceptor';
 
@@ -28,46 +32,34 @@ import { AuthInterceptor } from './services/auth-interceptor';
     RegisterPage,
     HomePage,
     ProfilePage,
-    ContactPage
+    ContactPage,
+    ForumPage,
+    AuctionsPage
   ],
-  import { HomePage } from './home-page/home-page';
-
-  import { AuctionsPage } from './auctions-page/auctions-page';
-  import { ContactPage } from './menu-item/contact-page/contact-page';
-  import { ForumPage } from './forum-page/forum-page';
-  import { ReviewComponent } from './Models/review/review';
-
-  import { MatCardModule } from '@angular/material/card';
-  import { MatIconModule } from '@angular/material/icon';
-  import { MatFormFieldModule } from '@angular/material/form-field';
-  import { MatInputModule } from '@angular/material/input';
-  import { MatButtonModule } from '@angular/material/button';
-
-@NgModule({
-    declarations: [App, LoginPage, RegisterPage, HomePage, ProfilePage, ContactPage, ForumPage, AuctionsPage],
-    imports: [
-      BrowserModule,
-      BrowserAnimationsModule,
-      AppRoutingModule,
-      FormsModule,
-      ReactiveFormsModule,
-      CommonModule,
-      HttpClientModule,
-      MatCardModule,
-      MatIconModule,
-      MatFormFieldModule,
-      MatInputModule,
-      MatButtonModule
-    ],
-    providers: [
-      provideBrowserGlobalErrorListeners(),
-      provideHttpClient(withInterceptorsFromDi()),
-      {
-        provide: HTTP_INTERCEPTORS,
-        useClass: AuthInterceptor,
-        multi: true
-      }
-    ],
-    bootstrap: [App],
-  })
-  export class AppModule { }
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    HttpClientModule,
+    MatCardModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReviewComponent
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
