a6728e98bd50aa530c82e556270030d31caf9ede
biancaivn18 <biancaivan18@yahoo.com>
2026-07-15 11:11:31 +0300

    Database relationships frontend (#63)
    * update models
    
    * Add bid model
    
    * Rezolvare neconcordanta nume variabile
    
    ---------
    
    Co-authored-by: Daniela BURGHIU <burghiudaniela2005@gmail.com>

--- frontend/LicitatieFrontend/src/app/models/item-model.ts
+++ frontend/LicitatieFrontend/src/app/models/item-model.ts
@@ -1,17 +1,25 @@
-import { StatusEnum } from "./status-enum";
+import { StatusEnum } from './status-enum';
+import { Category } from './user/categoryItem';
+import { User } from './user/user';
+import { Bid } from './bid/bid';
 
 export interface AuctionItem {
-  id: number;
-  name: string;
-  startPrice: number;
-  currentPrice: number;
-  category: string; //TODO category
-  description?: string;
-  location: string;
-  owner: string; //TODO user
-  winner?: string; //TODO user
-  status: StatusEnum;
-  startDate: Date;
-  endDate: Date;
+  ID: number;
+  Name: string;
+  StartPrice: number;
+  CurrentPrice: number;
+  Category: Category;
+  CategoryId: number;
+  WishingUsers: Array<User>;
+  Description?: string;
+  Location: string;
+  Owner: User;
+  OwnerId: number;
+  Winner?: User;
+  WinnerId?: number;
+  Status: StatusEnum;
+  StartDate: Date;
+  EndDate: Date;
+  BidList: Array<Bid>;
   //photoList ???
 }
