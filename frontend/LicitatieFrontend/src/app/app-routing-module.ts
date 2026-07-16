de12e56002a3acdc0a71a433103829846a836c92
Taranu Alexandru <alexandru.taranu@student.unitbv.ro>
2026-07-16 15:19:23 +0300

    Am adaugat doar modificarile pentru homepage

--- frontend/LicitatieFrontend/src/app/app-routing-module.ts
+++ frontend/LicitatieFrontend/src/app/app-routing-module.ts
@@ -32,19 +32,16 @@ const routes: Routes = [
   { path: 'profile-page', component: ProfilePage },
   { path: 'help-page', component: HelpPageComponent },
   { path: 'add-item', component: AddItemPage },
-
   { path: 'add', component: Add },
   { path: 'edit', component: Edit },
   { path: 'view', component: View },
   { path: 'action-item-page', component: AuctionItemPage },
-  { path: 'help-page', component: HelpPageComponent },
   { path: 'review-page', component: ReviewComponent },
-  { path: '', redirectTo: '/home-page', pathMatch: 'full' },
   { path: '**', redirectTo: '/home-page' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
