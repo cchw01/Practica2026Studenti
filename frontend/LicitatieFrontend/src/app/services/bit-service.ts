0e5d76941dad4e33deca240d00204a57a30123f0
allleeex21 <labesstefan24@stud.ase.ro>
2026-07-16 12:53:57 +0300

    Refactor bid model and implement BidService for API interactions

---
+++ frontend/LicitatieFrontend/src/app/services/bit-service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { BidDto, CreateBidDto } from '../Models/bid/bid'; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BidService {
+  
+ 
+  private readonly apiUrl = 'http://localhost:5153/api/Bid';
+
+  constructor(private http: HttpClient) { }
+
+  getAllBids(): Observable<BidDto[]> {
+    return this.http.get<BidDto[]>(this.apiUrl);
+  }
+
+ 
+  getBidsByItem(itemId: number): Observable<BidDto[]> {
+    return this.http.get<BidDto[]>(${this.apiUrl}/item/${itemId});
+  }
+
+
+  getBidById(id: number): Observable<BidDto> {
+    return this.http.get<BidDto>(${this.apiUrl}/${id});
+  }
+
+ 
+  addBid(bidData: CreateBidDto): Observable<BidDto> {
+    return this.http.post<BidDto>(this.apiUrl, bidData);
+  }
+
+  
+  deleteBid(id: number): Observable<void> {
+    return this.http.delete<void>(${this.apiUrl}/${id});
+  }
+}
