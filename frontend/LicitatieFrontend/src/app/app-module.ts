9b1a0c896be3515b4e2ee3df260e686be2815744
Hanna Katai <hanna01.katai@gmail.com>
2026-07-16 23:11:54 +0300

    ShareButton declaration move to import

--- frontend/LicitatieFrontend/src/app/app-module.ts
+++ frontend/LicitatieFrontend/src/app/app-module.ts
@@ -78,7 +78,6 @@ import { MatMenuModule } from '@angular/material/menu';
     Edit,
     View,
     AuctionDetail,
-    ShareListingButton,
     ForumPostDetails,
     CreateForumPost,
     AuctionDetail,
