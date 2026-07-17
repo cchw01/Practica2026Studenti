cd504925c99b4a55451c6f6b0ce3c14b5c80d611
Ionita_Vlad <ionita.vlad2006@gmail.com>
2026-07-17 13:33:47 +0300

    Move ReviewCreateDTO to ReviewDTO.cs

--- BackEnd/Backend/Backend/DTOs/ReviewDTO.cs
+++ BackEnd/Backend/Backend/DTOs/ReviewDTO.cs
@@ -15,4 +15,12 @@
 
         public DateTime ReviewDate { get; set; }
     }
+
+    public class ReviewCreateDTO
+    {
+        public int ReviewerId { get; set; }
+        public int ReviewedUserId { get; set; }
+        public float Rating { get; set; }
+        public string Comment { get; set; }
+    }
 }
