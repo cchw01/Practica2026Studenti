dd0ad3be638facaec2c8e6f2be77757e19e43917
BeatrixAva <beatriceavanu04@gmail.com>
2026-07-21 13:32:55 +0300

    add-item page

--- frontend/LicitatieFrontend/src/app/add-item-page/add-item-page.ts
+++ frontend/LicitatieFrontend/src/app/add-item-page/add-item-page.ts
@@ -50,11 +50,20 @@ export class AddItemPage implements OnInit {
   }
 
   ngOnInit(): void {
-    const currentUser = this.authService.getCurrentUser();
-    if (currentUser) {
-      this.currentUserId = +currentUser.id || this.currentUserId;
+    //verificam daca esti logat
+    if (!this.authService.isLoggedIn()) {
+      this.router.navigate(['/login-page']);
+      return;
     }
 
+    const currentUser = this.authService.getCurrentUser();
+
+    if (!currentUser || currentUser.role !== 'User') {
+      alert('Doar utilizatorii obișnuiți pot adăuga licitații.');
+      this.router.navigate(['/profile-page']);
+      return;
+    }
+    this.currentUserId = +currentUser.id || this.currentUserId;
     this.categoryService.getCategories().subscribe({
       next: (cats) => (this.categories = cats),
       error: (err) => console.error('Could not load categories', err),
@@ -136,7 +145,11 @@ export class AddItemPage implements OnInit {
       },
       error: (err) => {
         this.isError = true;
-        const errorMsg = err?.error?.message || err?.error || err?.message || 'A apărut o eroare la publicarea itemului.';
+        const errorMsg =
+          err?.error?.message ||
+          err?.error ||
+          err?.message ||
+          'A apărut o eroare la publicarea itemului.';
         this.message = typeof errorMsg === 'string' ? errorMsg : JSON.stringify(errorMsg);
         this.isSubmitting = false;
       },
