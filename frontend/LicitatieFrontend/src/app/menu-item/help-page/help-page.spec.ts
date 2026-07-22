014578cc15ad48a2cf5e57d3eb87c94e531a8c0a
Catalinpa19 <palade.catalin30@yahoo.com>
2026-07-22 14:49:21 +0300

    Revert "Merge pull request #188 from cchw01/AI_create"
    This reverts commit 5f0a70ef953b55d206b92180b76eb4fddf902561, reversing
    changes made to 69d79394b875765060f454efa576ef534300693a.

--- frontend/LicitatieFrontend/src/app/menu-item/help-page/help-page.spec.ts
+++ frontend/LicitatieFrontend/src/app/menu-item/help-page/help-page.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'; // <--- Asigură-te că ai importul acesta sus
 import { HelpPageComponent } from './help-page';
 
 describe('HelpPageComponent', () => {
@@ -9,7 +10,10 @@ describe('HelpPageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HelpPageComponent],
-      imports: [ReactiveFormsModule],
+      imports: [
+        ReactiveFormsModule,
+        CommonModule, // <--- Și adaugă-l aici în lista de imports
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HelpPageComponent);
